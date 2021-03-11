eb63592d4524540240568aa6d95f23957fb95618
it-dreams <rahulraj.holi888@gmail.com>
2021-03-11 23:28:43 +0530

    Add components

---
+++ src/app/component/core-components/mission-detail/mission-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mission-detail',
+  templateUrl: './mission-detail.component.html',
+  styleUrls: ['./mission-detail.component.scss']
+})
+export class MissionDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/component/sidebar/sidebar.component.ts
+++ src/app/component/sidebar/sidebar.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  title = 'Filters';
+
   constructor() { }
 
   ngOnInit(): void {
