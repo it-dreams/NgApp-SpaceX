bf7f3f9055d4e36ec556bd354633e7faaa99315a
it-dreams <rahulraj.holi888@gmail.com>
2021-03-11 23:29:34 +0530

    add angular universal for SSR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,26 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { SidebarComponent } from './component/sidebar/sidebar.component';
+import { DetailsComponent } from './component/details/details.component';
+import { MissionDetailComponent } from './component/core-components/mission-detail/mission-detail.component';
+import { ButtonComponent } from './component/core-components/button/button.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    SidebarComponent
+    SidebarComponent,
+    DetailsComponent,
+    MissionDetailComponent,
+    ButtonComponent
   ],
   imports: [
-    BrowserModule,
-    NoopAnimationsModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    NoopAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
